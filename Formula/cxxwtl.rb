c8c1048f3311a98a97b82491ee6492724538a852
Watal M. Iwasaki <heavy.watal@gmail.com>
2018-01-07 00:15:22 +0900

    :fire: Limit cxxwtl to head-only

--- Formula/cxxwtl.rb
+++ Formula/cxxwtl.rb
@@ -1,8 +1,6 @@
 class Cxxwtl < Formula
   desc "🤘 wtl: Personal C++ header library"
   homepage "https://github.com/heavywatal/cxxwtl"
-  url "https://github.com/heavywatal/cxxwtl/archive/v0.1.0.tar.gz"
-  sha256 "eb6ab235073fb8133c5d67bef3758d4a83f599a710d3162eea07998eae576a26"
   head "https://github.com/heavywatal/cxxwtl.git"
 
   depends_on "cmake" => :build
