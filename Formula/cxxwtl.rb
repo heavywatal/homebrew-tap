5580ad08b0858f7238b44e732a188c3180c6c937
Watal M. Iwasaki <heavy.watal@gmail.com>
2018-01-06 13:54:09 +0900

    :wrench: Add dependencies

--- Formula/cxxwtl.rb
+++ Formula/cxxwtl.rb
@@ -1,11 +1,14 @@
 class Cxxwtl < Formula
   desc "🤘 wtl: Personal C++ header library"
   homepage "https://github.com/heavywatal/cxxwtl"
-  head "https://github.com/heavywatal/cxxwtl.git"
   url "https://github.com/heavywatal/cxxwtl/archive/v0.1.0.tar.gz"
   sha256 "eb6ab235073fb8133c5d67bef3758d4a83f599a710d3162eea07998eae576a26"
+  head "https://github.com/heavywatal/cxxwtl.git"
 
   depends_on "cmake" => :build
+  depends_on "boost" => :recommended
+  depends_on "eigen" => :optional
+  needs :cxx14
 
   def install
     system "cmake", ".", *std_cmake_args
