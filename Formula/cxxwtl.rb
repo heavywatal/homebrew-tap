7ed1f666859aa5640992c366ad980d3dd8de909a
Watal M. Iwasaki <heavy.watal@gmail.com>
2018-01-06 13:27:04 +0900

    :wrench: Set homepage and head; remove comments

--- Formula/cxxwtl.rb
+++ Formula/cxxwtl.rb
@@ -1,32 +1,14 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook.html
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Cxxwtl < Formula
   desc "🤘 wtl: Personal C++ header library"
-  homepage ""
+  homepage "https://github.com/heavywatal/cxxwtl"
+  head "https://github.com/heavywatal/cxxwtl.git"
   url "https://github.com/heavywatal/cxxwtl/archive/v0.1.0.tar.gz"
   sha256 "eb6ab235073fb8133c5d67bef3758d4a83f599a710d3162eea07998eae576a26"
 
   depends_on "cmake" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
     system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
-  end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test cxxwtl`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "make", "install"
   end
 end
