a4dd4f7d03e91a61db06da3a81c16cce738ad47c
Watal M. Iwasaki <heavy.watal@gmail.com>
2018-01-06 22:36:36 +0900

    :sparkles: Add sfmt-class formula

---
+++ Formula/sfmt-class.rb
@@ -0,0 +1,13 @@
+class SfmtClass < Formula
+  desc "🎲 SFMT wrapper class for C++"
+  homepage "https://github.com/heavywatal/sfmt-class"
+  head "https://github.com/heavywatal/sfmt-class.git"
+
+  depends_on "cmake" => :build
+  needs :cxx11
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+end
