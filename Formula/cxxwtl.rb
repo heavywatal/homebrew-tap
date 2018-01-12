f722a778bc3a48d62368a6bef8d27bad96e240fa
Watal M. Iwasaki <heavy.watal@gmail.com>
2018-01-13 01:53:52 +0900

    :sparkles: Add stable version

--- Formula/cxxwtl.rb
+++ Formula/cxxwtl.rb
@@ -1,6 +1,8 @@
 class Cxxwtl < Formula
   desc "🤘 wtl: Personal C++ header library"
   homepage "https://github.com/heavywatal/cxxwtl"
+  url "https://github.com/heavywatal/cxxwtl/archive/v0.1.0.tar.gz"
+  sha256 "62d60efe877603b12b8a7794b10dc1dc10fc71629a7c11d163e30fc146143452"
   head "https://github.com/heavywatal/cxxwtl.git"
 
   depends_on "cmake" => :build
