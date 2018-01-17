f597cfdfe91ba81619a84a067482cb6bcc9ac66b
Watal M. Iwasaki <heavy.watal@gmail.com>
2018-01-17 14:23:12 +0900

    :sparkles: Add blackthunnus

---
+++ Formula/blackthunnus.rb
@@ -0,0 +1,18 @@
+class Tumopp < Formula
+  desc "🐟 Individual-based simulator of pacific bluefin tuna"
+  homepage "https://github.com/heavywatal/blackthunnus"
+  head "https://github.com/heavywatal/blackthunnus.git"
+
+  depends_on "cmake" => :build
+  depends_on "cxxwtl"
+  depends_on "sfmt-class"
+  depends_on "boost"
+  needs :cxx14
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make"
+    system "ctest", "-V"
+    system "make", "install"
+  end
+end
