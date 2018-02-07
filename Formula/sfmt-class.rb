2eef3d81969f02571c4cafbb81de8e8382b1cc29
Watal M. Iwasaki <heavy.watal@gmail.com>
2018-02-07 19:40:02 +0900

    :arrow_up: sfmt-class v0.4.2

--- Formula/sfmt-class.rb
+++ Formula/sfmt-class.rb
@@ -1,15 +1,15 @@
 class SfmtClass < Formula
   desc "🎲 SFMT wrapper class for C++"
   homepage "https://github.com/heavywatal/sfmt-class"
-  url "https://github.com/heavywatal/sfmt-class/archive/v0.4.1.tar.gz"
-  sha256 "9983b78b4b95a68e9a407be90bec366725a5faa13342ffd30f445f272646feff"
+  url "https://github.com/heavywatal/sfmt-class/archive/v0.4.2.tar.gz"
+  sha256 "6a278dd3af25182daacf2c667a6880f1d86d536a23209bfef383b201e045979e"
   head "https://github.com/heavywatal/sfmt-class.git"
 
   depends_on "cmake" => :build
   needs :cxx11
 
   def install
-    system "git", "clone", "https://github.com/MersenneTwister-Lab/SFMT.git"
+    system "git", "clone", "--depth=1", "https://github.com/MersenneTwister-Lab/SFMT.git"
     system "cmake", ".", *std_cmake_args
     system "make"
     system "ctest", "-V"
