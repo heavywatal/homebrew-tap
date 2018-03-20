e9b2558aa7cc787fdade1e0ef9723c13313af451
Watal M. Iwasaki <heavy.watal@gmail.com>
2018-03-20 15:55:44 +0900

    :sparkles: Add versioned tar ball to blackthunnus

--- Formula/blackthunnus.rb
+++ Formula/blackthunnus.rb
@@ -1,6 +1,8 @@
 class Blackthunnus < Formula
   desc "🐟 Individual-based simulator of pacific bluefin tuna"
   homepage "https://github.com/heavywatal/blackthunnus"
+  url "https://github.com/heavywatal/blackthunnus/archive/v0.1.0.tar.gz"
+  sha256 "a3dfaede90ef8e67a634d6355e3f702d85b56313933ff4512e80a65128ba3e35"
   head "https://github.com/heavywatal/blackthunnus.git"
 
   depends_on "cmake" => :build
