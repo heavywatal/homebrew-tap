ed0d002117ff67d9efb2c11dc49bd879b892a0d6
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-08-06 17:52:40 +0900

    :arrow_up: tumopp v0.3.0

--- Formula/tumopp.rb
+++ Formula/tumopp.rb
@@ -1,8 +1,8 @@
 class Tumopp < Formula
   desc "🦀 Tumor growth simulation in C++"
   homepage "https://github.com/heavywatal/tumopp"
-  url "https://github.com/heavywatal/tumopp/archive/v0.2.1.tar.gz"
-  sha256 "88d76f63e83034db2c8a05f8e74e8e06aa8612251f0304a70757206f6118ea9a"
+  url "https://github.com/heavywatal/tumopp/archive/v0.3.0.tar.gz"
+  sha256 "e0a4fe10ca16798b6462ce66bd3825f60dfa0bfb0d3e440adb61ef5bc8b0a557"
   head "https://github.com/heavywatal/tumopp.git"
 
   depends_on "cmake" => :build
