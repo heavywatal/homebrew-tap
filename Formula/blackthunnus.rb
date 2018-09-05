1c6300d22e72cc66855c6ff2f690e0f9e13f2c88
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-09-05 22:12:53 +0900

    :arrow_up: blackthunnus v0.1.6

--- Formula/blackthunnus.rb
+++ Formula/blackthunnus.rb
@@ -1,8 +1,8 @@
 class Blackthunnus < Formula
   desc "🐟 Individual-based simulator of pacific bluefin tuna"
   homepage "https://github.com/heavywatal/blackthunnus"
-  url "https://github.com/heavywatal/blackthunnus/archive/v0.1.5.tar.gz"
-  sha256 "5d74b2d1ed6d267c8bb4706849b5be0cd47ab6e2dc518248fb5f34506e72fcd4"
+  url "https://github.com/heavywatal/blackthunnus/archive/v0.1.6.tar.gz"
+  sha256 "d607f3a31c91defd1ac0485042a70830efca4e641617621d447ea5a1afff0f69"
   head "https://github.com/heavywatal/blackthunnus.git"
 
   depends_on "cmake" => :build
