3c473d1f940843aa279e6c5a71ef266b5006f11a
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-09-13 14:51:38 +0900

    :arrow_up: clippson v0.2.0

--- Formula/clippson.rb
+++ Formula/clippson.rb
@@ -1,34 +1,41 @@
 class Clippson < Formula
   desc "📎 Helper library of clipp, command-line parser for C++"
   homepage "https://github.com/heavywatal/clippson"
-  url "https://github.com/heavywatal/clippson/archive/v0.1.0.tar.gz"
-  sha256 "856ec878664b9ff0e7812868ad4b8fa012ce8d389d2f7f3132caaa201cf7414a"
+  url "https://github.com/heavywatal/clippson/archive/v0.2.0.tar.gz"
+  sha256 "cfed1e978b65886ca28b1d6d37ca3bf824a3d9411cee6917bf8375dcb0c9801e"
   head "https://github.com/heavywatal/clippson.git"
 
   depends_on "cmake" => :build
   needs :cxx11
 
   def install
-    system "git", "clone", "--depth=1", "https://github.com/heavywatal/clipp.git"
-    system "git", "clone", "--depth=1", "https://github.com/heavywatal/json.git"
-    system "cmake", ".", *std_cmake_args
-    system "make", "-j#{ENV.make_jobs}"
-    system "make", "install"
+    unless File.exist?("clipp/.git")
+      system "git", "clone", "--depth=1", "https://github.com/heavywatal/clipp.git"
+      system "git", "clone", "--depth=1", "https://github.com/heavywatal/json.git"
+    end
+    mkdir "build" do
+      cmake_args = std_cmake_args
+      cmake_args << "-DBUILD_TESTING=0" << ".."
+      system "cmake", *cmake_args
+      system "make", "install"
+    end
   end
 
   test do
-    system "ctest", "-V"
     (testpath/"test.cpp").write <<~EOS
       #include <clippson/clippson.hpp>
+      #include <iostream>
 
       int main(int argc, char* argv[]) {
           bool help = false;
           int whoami = 24601;
-          auto cli = clipp::with_prefixes_short_long("-", "--",
-            wtl::option({"h", "help"}, &help),
-            wtl::option({"whoami"}, &whoami)
+          nlohmann::json vm;
+          auto cli = (
+            wtl::option(vm, {"-h", "--help"}, false),
+            wtl::option(vm, {"--whoami"}, 24601)
           );
           if (!clipp::parse(argc, argv, cli)) return 1;
+          std::cout << vm.dump(2) << std::endl;
           return 0;
       }
     EOS
