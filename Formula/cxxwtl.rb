737f07b8bb104f8edaac32d62a302589f7f1b9a8
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-09-18 16:30:55 +0900

    :arrow_up: cxxwtl v0.5.0

--- Formula/cxxwtl.rb
+++ Formula/cxxwtl.rb
@@ -1,8 +1,8 @@
 class Cxxwtl < Formula
   desc "🤘 wtl: Personal C++ header library"
   homepage "https://github.com/heavywatal/cxxwtl"
-  url "https://github.com/heavywatal/cxxwtl/archive/v0.4.0.tar.gz"
-  sha256 "e79722ccb286b80c11d60e5adc9d54113c3b7bc24683a436ac52a72980750300"
+  url "https://github.com/heavywatal/cxxwtl/archive/v0.5.0.tar.gz"
+  sha256 "8723a45ff3e4114442632b637484b38d1e8785ec2d642dd9f46b9d82f84c4a67"
   head "https://github.com/heavywatal/cxxwtl.git"
 
   depends_on "cmake" => :build
