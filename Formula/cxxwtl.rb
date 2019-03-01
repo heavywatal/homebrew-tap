cdce8cd455076b546f4c49102ceac0f9a2fa04e0
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2019-03-01 09:52:36 +0900

    :wrench: Revert explicit BUILD_TESTING=OFF

--- Formula/cxxwtl.rb
+++ Formula/cxxwtl.rb
@@ -11,7 +11,8 @@ class Cxxwtl < Formula
 
   def install
     mkdir "build" do
-      cmake_args = std_cmake_args << ".."
+      cmake_args = std_cmake_args
+      cmake_args << "-DBUILD_TESTING=OFF" << ".."
       system "cmake", *cmake_args
       system "make", "install"
     end
