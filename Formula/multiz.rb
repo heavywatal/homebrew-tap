3c15d3a5d297a9303e5cf8bbbce47ce8d37fe6a8
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2019-10-03 16:08:49 +0900

    :sparkles: Create formula for multiz

---
+++ Formula/multiz.rb
@@ -0,0 +1,17 @@
+class Multiz < Formula
+  desc "DNA multiple sequence aligner"
+  homepage "https://www.bx.psu.edu/miller_lab/"
+  url "https://github.com/heavywatal/multiz/archive/20191003.tar.gz"
+  sha256 "1fcda7e52697116b23f18f73fa9c7ee31c33ad4d8f107b10601680037a8722ec"
+  head "https://github.com/heavywatal/multiz.git"
+
+  def install
+    system "make"
+    system "make", "DESTDIR=#{prefix}", "install"
+  end
+
+  test do
+    system "echo", bin/"multiz"
+    system "which", "multiz"
+  end
+end
