bce58a090c2f3152cc064fb3feb614b47ed902d8
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2021-10-20 14:31:46 +0900

    :arrow_up: Follow upstream formula updates

--- Formula/igvtools.rb
+++ Formula/igvtools.rb
@@ -1,29 +1,27 @@
 class Igvtools < Formula
   desc "Interactive Genomics Viewer"
   homepage "https://www.broadinstitute.org/software/igv"
-  url "https://data.broadinstitute.org/igv/projects/downloads/2.11/IGV_2.11.1.zip"
-  sha256 "71e9dc22d161b1747262a77c5053a224e2d279b2bbe4a0bdd64ab3a095fc88c7"
+  url "https://data.broadinstitute.org/igv/projects/downloads/2.11/IGV_2.11.2.zip"
+  sha256 "d036cdde3a99d1d415b78ed03c9aedf7b2b9fbb2da61e12dade0110956d33382"
+  license "MIT"
 
   livecheck do
     url "https://software.broadinstitute.org/software/igv/download"
     regex(/href=.*?IGV[._-]v?(\d+(?:\.\d+)+)\.zip/i)
   end
 
-  bottle :unneeded
-
-  depends_on "openjdk@11"
+  depends_on "openjdk"
 
   def install
     inreplace ["igv.sh", "igvtools"], /^prefix=.*/, "prefix=#{libexec}"
     bin.install "igv.sh" => "igv"
     bin.install "igvtools"
     libexec.install "igv.args", "lib"
-    bin.env_script_all_files libexec, JAVA_HOME: Formula["openjdk@11"].opt_prefix
+    bin.env_script_all_files libexec, Language::Java.overridable_java_home_env
   end
 
   test do
     assert_match "Usage:", shell_output("#{bin}/igvtools")
-    assert_match "org/broad/igv/ui/IGV.class",
-                 shell_output("#{Formula["openjdk@11"].bin}/jar tf #{libexec}/lib/igv.jar")
+    assert_match "org/broad/igv/ui/IGV.class", shell_output("#{Formula["openjdk"].bin}/jar tf #{libexec}/lib/igv.jar")
   end
 end
