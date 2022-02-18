bb080320e02e823618476d927089006d28153636
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2022-02-18 13:02:11 +0900

    :bug: Fix depends_on "clapack"

--- Formula/phast.rb
+++ Formula/phast.rb
@@ -5,7 +5,7 @@ class Phast < Formula
   sha256 "8100f6582008c5de46d2de05cee038f0f1ca3a50147031da1bc5e8744883cbe4"
   head "https://github.com/heavywatal/phast.git"
 
-  depends_on :clapack if OS.linux?
+  depends_on "clapack" if OS.linux?
 
   def install
     inreplace "src/Makefile", "${PWD}", "#{buildpath}/src"
