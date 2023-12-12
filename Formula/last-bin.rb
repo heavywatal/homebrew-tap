636f92a6654c0147e9317e3ba4b65b2be773e708
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2023-12-12 16:37:30 +0900

    :arrow_up: Bump last-bin to 1519

--- Formula/last-bin.rb
+++ Formula/last-bin.rb
@@ -1,8 +1,8 @@
 class LastBin < Formula
   desc "Auxiliary python scripts included in LAST repository"
   homepage "https://gitlab.com/mcfrith/last"
-  url "https://gitlab.com/mcfrith/last/-/archive/1454/last-1454.tar.gz"
-  sha256 "9a47330b38876ed9e4909004216be686bb6bc67fb301c103b5ef07cacd10dc7d"
+  url "https://gitlab.com/mcfrith/last/-/archive/1519/last-1519.tar.gz"
+  sha256 "d2408c45560dad37f6761c9e9f9a8b3cfccceb4783177362bc4d45bcdecec923"
   head "https://gitlab.com/mcfrith/last.git", branch: "master"
 
   def install
