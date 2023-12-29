736e91cddf98f17ff598a73bac7e94b4098f9159
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2023-12-29 23:53:22 +0900

    :arrow_up: Bump last-bin to 1522 with tag style

--- Formula/last-bin.rb
+++ Formula/last-bin.rb
@@ -1,9 +1,9 @@
 class LastBin < Formula
   desc "Auxiliary python scripts included in LAST repository"
   homepage "https://gitlab.com/mcfrith/last"
-  url "https://gitlab.com/mcfrith/last/-/archive/1519/last-1519.tar.gz"
-  sha256 "d2408c45560dad37f6761c9e9f9a8b3cfccceb4783177362bc4d45bcdecec923"
-  head "https://gitlab.com/mcfrith/last.git", branch: "master"
+  url "https://gitlab.com/mcfrith/last.git",
+    tag: "1522"
+  head "https://gitlab.com/mcfrith/last.git", branch: "main"
 
   def install
     bin.install Dir["bin/*"]
