2ee3cdeb51b3bbc52e0c56155e8be03d9246653b
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2024-06-07 13:31:29 +0900

    :arrow_up: tumopp v0.8.7.1

--- Formula/tumopp.rb
+++ Formula/tumopp.rb
@@ -2,7 +2,7 @@ class Tumopp < Formula
   desc "Tumor growth simulation in C++"
   homepage "https://github.com/heavywatal/tumopp"
   url "https://github.com/heavywatal/tumopp.git",
-      tag: "v0.8.7"
+      tag: "v0.8.7.1"
   head "https://github.com/heavywatal/tumopp.git"
 
   depends_on "cmake" => :build
