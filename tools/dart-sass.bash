c8e4ae16a665c56461703d28b8bb4ceadee56606
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2024-10-17 13:51:18 +0900

    :art: Automate updating dart-sass.rb
    Change file extension to avoid false positive warning on heredoc

---
+++ tools/dart-sass.bash
@@ -0,0 +1,53 @@
+#!/bin/sh
+set -eu
+HERE=$(dirname "$0")
+version=$1
+repo=sass
+formula=dart-sass
+prefix="https://github.com/${repo}/${formula}/releases/download"
+
+sha256() {
+  "${HERE}/sha256.sh" "${prefix}/${version}/${formula}-${version}-${arch}.tar.gz"
+}
+
+arch=macos-x64 sha256_macos_x64=$(sha256)
+arch=macos-arm64 sha256_macos_arm64=$(sha256)
+arch=linux-x64 sha256_linux_x64=$(sha256)
+
+cat >"${HERE}/../Formula/dart-sass.rb" <<EOS
+class DartSass < Formula
+  desc "Dart implementation of Sass"
+  homepage "https://sass-lang.com/dart-sass/"
+  version "${version}"
+  head "https://github.com/sass/dart-sass.git"
+
+  livecheck do
+    url :head
+    regex(/^(\d\.[\d\.]+)$/i)
+  end
+
+  if OS.mac?
+    on_intel do
+      url "https://github.com/sass/dart-sass/releases/download/#{version}/dart-sass-#{version}-macos-x64.tar.gz"
+      sha256 "${sha256_macos_x64}"
+    end
+    on_arm do
+      url "https://github.com/sass/dart-sass/releases/download/#{version}/dart-sass-#{version}-macos-arm64.tar.gz"
+      sha256 "${sha256_macos_arm64}"
+    end
+  else
+    url "https://github.com/sass/dart-sass/releases/download/#{version}/dart-sass-#{version}-linux-x64.tar.gz"
+    sha256 "${sha256_linux_x64}"
+  end
+
+  def install
+    libexec.install "sass"
+    libexec.install "src"
+    bin.install_symlink libexec/"sass"
+  end
+
+  test do
+    system "#{bin}/sass", "--version"
+  end
+end
+EOS
--- tools/sha256.sh
+++ tools/sha256.sh
@@ -4,4 +4,4 @@ HERE=$(dirname "$0")
 URL=$1
 FILE=${HERE}/${URL##*/}
 [[ -s "${FILE}" ]] || curl -LO "${URL}" --output-dir "${HERE}"
-shasum -a 256 "${FILE}"
+shasum -a 256 "${FILE}" | hck -f1
