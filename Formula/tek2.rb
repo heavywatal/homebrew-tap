7d231de0cb3fba02401bbc9d2a46244870bc7136
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2024-10-30 00:55:39 +0900

    :arrow_up: tek2 v0.2.2

--- Formula/tek2.rb
+++ Formula/tek2.rb
@@ -2,8 +2,7 @@ class Tek2 < Formula
   desc "Population Genetic Simulation Framework of Transposable Elements"
   homepage "https://github.com/heavywatal/tek2"
   url "https://github.com/heavywatal/tek2.git",
-      tag: "v0.2.1"
-  sha256 "6de9bce36d2b363ba8d2f9752a1568f029938b8ba2aed0f98c95d86c23e58420"
+      tag: "v0.2.2"
   head "https://github.com/heavywatal/tek2.git"
 
   depends_on "cmake" => :build
