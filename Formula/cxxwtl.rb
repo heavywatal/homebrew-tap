c1649786c32c4cb79baee71af8d26123b0615289
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2024-11-20 20:49:37 +0900

    :arrow_up: cxxwtl v0.9.2

--- Formula/cxxwtl.rb
+++ Formula/cxxwtl.rb
@@ -2,7 +2,7 @@ class Cxxwtl < Formula
   desc "Personal C++ header library"
   homepage "https://github.com/heavywatal/cxxwtl"
   url "https://github.com/heavywatal/cxxwtl.git",
-      tag: "v0.9.1"
+      tag: "v0.9.2"
   head "https://github.com/heavywatal/cxxwtl.git"
 
   depends_on "cmake" => :build
