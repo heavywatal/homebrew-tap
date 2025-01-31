26272cff9566d9d5a2c102966a6879fab71b1641
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2025-01-31 13:56:24 +0900

    jbrowse2 3.0.1

--- Casks/jbrowse2.rb
+++ Casks/jbrowse2.rb
@@ -1,6 +1,6 @@
 cask "jbrowse2" do
-  version "2.18.0"
-  sha256 "bb57344ebe6f69d393c21dba200ec01c67d408551ece6159a2797626752b6ce6"
+  version "3.0.1"
+  sha256 "3e1a1f3dd2be24e9d405dbd8534830526a675383e5837048199b51672179e63e"
 
   url "https://github.com/GMOD/jbrowse-components/releases/download/v#{version}/jbrowse-desktop-v#{version}-mac.dmg",
       verified: "github.com/GMOD/jbrowse-components/"
