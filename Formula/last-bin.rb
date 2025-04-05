d348a451af35cd5778c6365c681bf63320859dbe
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2025-04-05 17:22:25 +0900

    :arrow_up: last-bin 1638

--- Formula/last-bin.rb
+++ Formula/last-bin.rb
@@ -2,8 +2,8 @@ class LastBin < Formula
   desc "Auxiliary python scripts included in LAST repository"
   homepage "https://gitlab.com/mcfrith/last"
   url "https://gitlab.com/mcfrith/last.git",
-    tag:      "1608",
-    revision: "07a5d4e2268d6f372aaf636058db5b37876b2068"
+    tag:      "1638",
+    revision: "eaca96d2f70a7cb1e660010efd28d2858ef0e7fe"
   head "https://gitlab.com/mcfrith/last.git", branch: "main"
 
   def install
