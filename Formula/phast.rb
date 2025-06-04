6f275902a5f6a97eca923860358194c8779bfc8f
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2025-06-04 17:37:33 +0900

    :arrow_up: phast v1.8

--- Formula/phast.rb
+++ Formula/phast.rb
@@ -1,8 +1,8 @@
 class Phast < Formula
   desc "Phylogenetic Analysis with Space/Time Models"
   homepage "http://compgen.cshl.edu/phast/"
-  url "https://github.com/heavywatal/phast/archive/refs/tags/v1.7.tar.gz"
-  sha256 "710e71f201cfb6c263ccb9ab38f4e63bc50f2fe16405c1e4562f6d737c7ca9eb"
+  url "https://github.com/heavywatal/phast/archive/refs/tags/v1.8.tar.gz"
+  sha256 "cf940869102cff48bdfbfab6b23c6515eae743959649903174c7e24f21d26b50"
   head "https://github.com/heavywatal/phast.git", branch: "dev"
 
   def install
