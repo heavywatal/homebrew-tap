3522e8437e2ff4fe36aab78ac790c154e5cb9d9a
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2025-07-04 13:17:37 +0900

    :arrow_up: pcglite v0.2.1

--- Formula/pcglite.rb
+++ Formula/pcglite.rb
@@ -2,7 +2,7 @@ class Pcglite < Formula
   desc "Lightweight subset of pcg-cpp, random number generator for C++"
   homepage "https://github.com/heavywatal/pcglite"
   url "https://github.com/heavywatal/pcglite.git",
-      tag: "v0.2.0"
+      tag: "v0.2.1"
   head "https://github.com/heavywatal/pcglite.git"
 
   depends_on "cmake" => :build
