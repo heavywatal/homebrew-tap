cf9a05f7918a3c6909c2c22e88e48b81d5c61016
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2025-09-09 14:19:13 +0900

    chore: plink2bin 20250824 alpha 6.23

--- Formula/plink2bin.rb
+++ Formula/plink2bin.rb
@@ -1,16 +1,16 @@
 class Plink2bin < Formula
   desc "Whole genome association analysis toolset"
   homepage "https://www.cog-genomics.org/plink/2.0/"
-  version "20250819"
+  version "20250824"
   head "https://github.com/chrchang/plink-ng.git", branch: "master"
   conflicts_with "brewsci/bio/plink2", because: "both install plink2"
 
   if OS.mac?
     url "https://s3.amazonaws.com/plink2-assets/alpha6/plink2_mac_arm64_#{version}.zip"
-    sha256 "982a229fc8ddce1b7f58e24e009ed98b41e75735b97aa7d805f9bb1e6fe1cc8d"
+    sha256 "f3fa35060398d8d8db63c34e6c2a4098e8a9875cb22e4ba8bd1955546930281e"
   else
     url "https://s3.amazonaws.com/plink2-assets/alpha6/plink2_linux_avx2_#{version}.zip"
-    sha256 "ba688f529f345828e829541a9b949d67330b211525994783151819052f2b048b"
+    sha256 "80824a22c0e92c09583ec5da89c0bfc0fb0c1bf525031b77246fa2fdeb5f9f2d"
   end
 
   def install
