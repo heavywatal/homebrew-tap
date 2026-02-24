46bc1d7e041cfddbb0faac093fb167ddb019d3f0
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2026-02-24 15:00:05 +0900

    chore: phast 1.9.7

--- Formula/phast.rb
+++ Formula/phast.rb
@@ -1,9 +1,9 @@
 class Phast < Formula
   desc "Phylogenetic Analysis with Space/Time Models"
   homepage "http://compgen.cshl.edu/phast/"
-  url "https://github.com/heavywatal/phast/archive/refs/tags/v1.9.4.tar.gz"
-  sha256 "eb6c71aa71843093f94770511616f987a35dbb8805fb2cfd635b8ada72fede70"
-  head "https://github.com/heavywatal/phast.git", branch: "version-1.9.4"
+  url "https://github.com/heavywatal/phast/archive/refs/tags/v1.9.7.tar.gz"
+  sha256 "78b86fc4234444ed15010c33208cfe48313205962f498bcfe91ece0dd1876892"
+  head "https://github.com/heavywatal/phast.git", branch: "version-1.9.7"
 
   depends_on "cmake" => :build
   depends_on "pcre" => :build
