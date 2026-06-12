942b368b76a0b73b0958a180add72592990118f2
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2026-06-12 10:20:46 +0900

    chore: tekka 0.10.0

--- Formula/tekka.rb
+++ Formula/tekka.rb
@@ -1,8 +1,8 @@
 class Tekka < Formula
   desc "Individual-based simulator of pacific bluefin tuna"
   homepage "https://github.com/heavywatal/tekka"
-  url "https://github.com/heavywatal/tekka/archive/refs/tags/v0.9.0.tar.gz"
-  sha256 "b9bd31dbf42b49c3d523e158814025ac78c95c4c62a2bf9e802db3bf3ffae609"
+  url "https://github.com/heavywatal/tekka/archive/refs/tags/v0.10.0.tar.gz"
+  sha256 "40f7f751fd5aae734c24440cc77180eac7873163a05cd1b790a66c8650492de6"
   head "https://github.com/heavywatal/tekka.git"
 
   depends_on "cmake" => :build
