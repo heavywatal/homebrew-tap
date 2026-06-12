6f7ee206e7533285d158b6248d69bcbcdbe4b2ef
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2026-06-12 10:20:09 +0900

    chore: cxxwtl 0.11.3

--- Formula/cxxwtl.rb
+++ Formula/cxxwtl.rb
@@ -1,8 +1,8 @@
 class Cxxwtl < Formula
   desc "Personal C++ header library"
   homepage "https://github.com/heavywatal/cxxwtl"
-  url "https://github.com/heavywatal/cxxwtl/archive/refs/tags/v0.11.2.tar.gz"
-  sha256 "39f432ccc56c2fa2c56d1c93a9a8ee127875a6672373f2c7c0b81295eebe5d11"
+  url "https://github.com/heavywatal/cxxwtl/archive/refs/tags/v0.11.3.tar.gz"
+  sha256 "b8df5b253ddb7d8772a432deef7fa5d92eed05e16720c9a1eb14e630a47aea70"
   head "https://github.com/heavywatal/cxxwtl.git"
 
   depends_on "cmake" => :build
