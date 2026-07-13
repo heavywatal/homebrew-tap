e09c3d1ed0302a0c8a3886d6e78a27836dd0d032
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2026-07-13 17:07:17 +0900

    chore: tumopp 0.9.3

--- Formula/tumopp.rb
+++ Formula/tumopp.rb
@@ -1,8 +1,8 @@
 class Tumopp < Formula
   desc "Tumor growth simulation in C++"
   homepage "https://github.com/heavywatal/tumopp"
-  url "https://github.com/heavywatal/tumopp.git",
-    tag: "v0.9.2"
+  url "https://github.com/heavywatal/tumopp/archive/refs/tags/v0.9.3.tar.gz"
+  sha256 "c87b9d2c440b0c5c951016650b59b051261007f13b05715219ae3cc63da7991c"
   head "https://github.com/heavywatal/tumopp.git"
 
   depends_on "cmake" => :build
@@ -12,7 +12,7 @@ class Tumopp < Formula
 
   def install
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args,
-      "-DFETCHCONTENT_FULLY_DISCONNECTED=OFF"
+      "-DHOMEBREW_ALLOW_FETCHCONTENT=ON"
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
